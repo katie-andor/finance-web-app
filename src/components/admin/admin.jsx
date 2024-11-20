08f9f3881578f9e703a3bd3234e06d998582721c
Katie Andor <superkga5678@gmail.com>
2024-11-20 14:25:04 -0500

    admin auth

--- src/components/admin/admin.jsx
+++ src/components/admin/admin.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { doSignOut } from "../../firebase/auth";
 
 const Admin = () => {
-  return (
-    <div>Admin</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Admin
+  const handleSignOut = async () => {
+    try {
+      await doSignOut();
+      navigate("/admin/login");
+    } catch (error) {
+      console.error("Error during sign-out: ", error);
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-full max-w-sm">
+        <h1 className="text-center text-2xl font-bold">Admin Dashboard</h1>
+        <button
+          onClick={handleSignOut}
+          className="w-full mt-4 p-2 bg-red-600 text-white rounded"
+        >
+          Sign Out
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Admin;
